886ccd96c1d2c448686a4e1eb3bde1be1fbb8c5c
michealyoussef <michealysha@yahoo.com>
2015-10-18 17:04:59 +0200

    last

--- WindowsFormsApplication1/WindowsFormsApplication1/histogramdrawing.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/histogramdrawing.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,21 +15,31 @@ namespace WindowsFormsApplication1
 
         public Histogramdrawing(int maxintensity)
         {
-            Rarray = new int[maxintensity+1];
-            Garray = new int[maxintensity+1];
-            Barray = new int[maxintensity+1];
+            Rarray = new int[256];
+            Garray = new int[256];
+            Barray = new int[256];
         }
-        public void drawing(bufferedLockBitmap input_image)
+        public void drawing(Bitmap input_image)
         {
-
-
+            int R, G, B;
+            for (int i = 0; i < input_image.Height; i++)
+            {
+                for (int j = 0; j < input_image.Width; j++)
+                {
+                    R = input_image.GetPixel(j, i).R;
+                    Rarray[R]++;
+                    G = input_image.GetPixel(j, i).G;
+                    Garray[G]++;
+                    B = input_image.GetPixel(j, i).B;
+                    Barray[B]++;
+                }
+            }
         }
-
         public void get_series_for_chart_component()
         {
-            // return series array to be rendered in chart in ui
+            // return series array to be rendered in chart
         }
 
-        
+
     }
 }
